c17d3707525ef35b5d3c266b0442f28961de3fd9
Himanshi <shimanshi9911@gmail.com>
2021-09-17 20:35:24 +0530

    added initial files

---
+++ app.js
@@ -0,0 +1,27 @@
+let client1 = document.querySelector('.client-1');
+let client2 = document.querySelector('.client-2');
+
+window.setInterval(function(){
+  client1.classList.toggle("hide");
+  client2.classList.toggle("hide");
+},3000)
+
+
+
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+const obj1 = document.getElementById("value1");
+const obj2 = document.getElementById("value2");
+animateValue(obj1, 0, 100, 2000);
+animateValue(obj2, 0, 500, 2000);
